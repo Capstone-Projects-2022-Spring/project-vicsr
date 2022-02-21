7e413a9bfe153d141eefba36a9836451d7d34453
Cecily Devaney <tul06320@temple.edu>
2022-02-20 19:06:42 -0500

    Updated to have components as Django apps per Django best practices

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'VICSR_Server.apps.VicsrServerConfig',
+    'AccountManagement',
+    'DocumentManagement',
+    'VocabularyManagement'
 ]
 
 MIDDLEWARE = [
