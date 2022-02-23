18ae1f195a2deacb8c82a57946bc7630ba027310
Alyssa <tui91984@temple.edu>
2022-02-23 12:15:54 -0500

    settings updates

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('', include('AccountManagement.urls')),
     path('admin/', admin.site.urls),
+    path('', include('AccountManagement.urls')),
 ]
