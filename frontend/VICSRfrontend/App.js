c254d1c0ed107fc49a65e9a22f4d702001a776f4
bee-mcc <tuj03791@temple.edu>
2022-02-24 12:37:33 -0500

    Added helpful comments and styling

--- frontend/VICSRfrontend/App.js
+++ frontend/VICSRfrontend/App.js
@@ -9,13 +9,12 @@ const HelloWorldApp = () => {
     const [password, setPassword] = useState('');
 
     return (
-        <View
-          style={{
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "center"
-          }}>
+        //Basically the head of the document
+        <View style={styles.container}>
+            {/* Header Text */}
             <Text>Please login below</Text>
+            {/* A new view(like a div) associated with a new stylesheet */}
+            {/* New component, collect user inputs*/}
             <View style={styles.inputView}>
               <TextInput
                 style={styles.TextInput}
@@ -24,7 +23,6 @@ const HelloWorldApp = () => {
                 onChangeText={(email) => setEmail(email)}
               />
             </View>
-
             <View style={styles.inputView}>
               <TextInput
                 style={styles.TextInput}
@@ -37,19 +35,30 @@ const HelloWorldApp = () => {
         </View>
       )
 }
+{/* Set of all stylesheets */}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#5FE88D',
     alignItems: 'center',
     justifyContent: 'center',
    },
+    inputView: {
+        backgroundColor: "#FFC0CB",
+        borderRadius: 30,
+        width: "70%",
+        height: 45,
+        marginBottom: 20,
 
-   image :{
-    marginBottom: 40
+        alignItems: "center",
+    },
 
-  }
-
+    TextInput: {
+        height: 50,
+        flex: 1,
+        padding: 10,
+        marginLeft: 20,
+    }
 });
 
 export default HelloWorldApp;
