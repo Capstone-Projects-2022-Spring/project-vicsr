b30c3f63aa5dcf570a40748561f60db36b5c7ddc
Cecily Devaney <tul06320@temple.edu>
2022-03-01 18:48:52 -0500

    Continued document model and started studyset model and document words model

--- backend/DocumentManagement/models.py
+++ backend/DocumentManagement/models.py
@@ -12,14 +12,26 @@ from django.contrib.auth.models import User
 class Document(models.Model):
     owner_id = models.ForeignKey(User, on_delete=models.CASCADE)
     filename = models.CharField(max_length=30)
+    date_added = models.DateTimeField(auto_now_add=True)
+    #need to review file further, and need to check how to get file size
+    file = models.FileField()
+    size = models.IntegerField()
+    language = models.CharField(max_length=50)
 
 
+""" commenting out -- not relevant for milestone 1 and may need reworking 
+leaving as starting point when returning
+
+# may need to move this to vocab management
 class DocumentWords(models.Model):
     generated_by = models.ForeignKey(Document, on_delete=models.CASCADE)
     word = models.CharField(max_length=65)
+    ##coordinates from tesseract - may want to change
+    left = models.IntegerField()
+    top = models.IntegerField()
+    width = models.IntegerField()
+    height = models.IntegerField()
+    """
 
-
-#class StudySet(models.Model):
-   # generated_by = models.ForeignKey(Document, on_delete=models.CASCADE, null=True)
 
 # class DocumentList(models.Model):
--- backend/VocabularyManagement/models.py
+++ backend/VocabularyManagement/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from backend.DocumentManagement.models import Document
 
 # Create your models here.
 
@@ -8,3 +10,11 @@ from django.db import models
 #Highlight exraction
 #Vocabulary
 #Study Set
+
+
+class StudySet(models.Model):
+    owner_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    generated_by = models.ForeignKey(Document, on_delete=models.CASCADE, null=True)
+    title = models.CharField(max_length=30)
+    date_added = models.DateTimeField(auto_now_add=True)
+
