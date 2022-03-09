b4293b049fa8cd16fb13724645990da8ab76d96d
Shuang71671 <77736227+Shuang71671@users.noreply.github.com>
2022-03-09 17:58:30 -0500

    Update views.py

--- backend-vicsr/AccountManagement/views.py
+++ backend-vicsr/AccountManagement/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from django.contrib.auth.models import User, auth
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import get_user_model
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth import login, logout
 from rest_framework.generics import CreateAPIView
 from .serializers import AccountSerializer
 from rest_framework.response import Response
@@ -29,6 +31,36 @@ class AccountAPIView(CreateAPIView):
             status=status.HTTP_201_CREATED,
             headers=headers
         )
+
+    def signup_view(request):
+        if request.method == 'POST':
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                user = form.save()
+                #log the user in
+                login(request, user)
+                return redirect('articles:list')
+            else:
+            form = UserCreationForm()
+        return render(request, 'accounts/signup.html', {'form':form})
+
+    def login_view(request):
+        if request.method == 'POST':
+            form = AuthenticationForm(data=request.POST)
+            if form.is_valid():
+                #log in the user
+                user = form.get_user()
+                login(request,user)
+                return redirect('articles:list')
+        else:
+            form = AuthenticationForm()
+        return render(request, 'accounts/login.html', {'form':form})
+
+    def logout_view(request):
+        if request.method == 'POST':
+            logout(request)
+            return redirect('articles:list')
+
 
     def logout(self, request, format=None):
         # delete user token, something like: request.user.auth_token.delete()
