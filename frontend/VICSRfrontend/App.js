44ead70104d0886b6b37e427e93a33d8e833fff6
bee-mcc <tuj03791@temple.edu>
2022-03-08 20:02:50 -0500

    prototype of DocumentView page made and linked to app

--- frontend/VICSRfrontend/App.js
+++ frontend/VICSRfrontend/App.js
@@ -1,5 +1,6 @@
 import Login from "./components/Login.js"
 import Register from "./components/Register.js"
+import DocumentView from "./components/DocumentView.js"
 
 import React, { useState } from 'react';
 
@@ -17,6 +18,7 @@ const App = () => {
             <Stack.Navigator initialRouteName = "Login">
                 <Stack.Screen name="Login" component={Login} />
                 <Stack.Screen name= "Register" component={Register} />
+                <Stack.Screen name="DocumentView" component={DocumentView} />
             </Stack.Navigator>
         </NavigationContainer>
 
---
+++ frontend/VICSRfrontend/components/DocumentView.js
@@ -0,0 +1,95 @@
+import React, {useState} from "react"
+import {FlatList, StyleSheet, Text, TouchableOpacity, View} from "react-native";
+
+const DATA = [
+    {
+    url: " 1",
+    name: "Fake document 1"
+    },
+    {
+    url: " 2",
+    name: "Fake document 2"
+    },
+    {
+    url: " 3",
+    name: "Fake document 3"
+    },
+]
+
+//Item "object"
+const Item = ({ item, onPress, backgroundColor, textColor }) => (
+  <TouchableOpacity onPress={onPress} style={[styles.item, backgroundColor]}>
+    <Text style={[styles.name, textColor]}>{item.name}</Text>
+  </TouchableOpacity>
+);
+
+function DocumentView({navigation}){
+    //vars to keep track of pressed or not pressed
+    const [selectedId, setSelectedId] = useState(null);
+
+    //Function to render the item(from item object)
+    const renderItem = ({ item }) => {
+        const backgroundColor = item.url === selectedId ? "#e85f76" : "#E85FBA";
+        const color = item.url === selectedId ? 'white' : 'black';
+
+        return (
+            <Item
+                item={item}
+                onPress={() => setSelectedId(item.url)}
+                backgroundColor={{backgroundColor}}
+                textColor={{color}}
+            />
+        );
+    }
+    return(
+        <View style={styles.container}>
+            <View style={styles.header_list}>
+                {/*List component. Data prop gets passed the array of data. renderItem prop a function which renders items*/}
+              <FlatList
+                data={DATA}
+                renderItem={renderItem}
+                keyExtractor={item => item.url}
+                extraData={selectedId}
+                horizontal={true}
+              />
+            </View>
+            <View style={styles.my_canvas}>
+
+            </View>
+
+        </View>
+    )
+}
+
+{/* Set of all stylesheets for this page */}
+const styles = StyleSheet.create({
+  container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+   },
+    header_list: {
+        flex: 1.5,
+        backgroundColor: "#5FE88D",
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    item: {
+        backgroundColor: '#e85f76',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+        width: 160,
+        height: 70
+    },
+    name: {
+        fontSize: 14
+    },
+    my_canvas: {
+      flex: 9,
+      backgroundColor: "#ebebeb"
+
+    }
+});
+
+export default DocumentView
