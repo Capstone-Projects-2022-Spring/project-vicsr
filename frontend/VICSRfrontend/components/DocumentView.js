ed49d3b519bda8e94ce586c1dd5f4033ad6ceb12
bee-mcc <tuj03791@temple.edu>
2022-03-08 20:52:43 -0500

    added floating button and modal to scan/add documents

--- frontend/VICSRfrontend/components/DocumentView.js
+++ frontend/VICSRfrontend/components/DocumentView.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react"
-import {FlatList, StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import {FlatList, Modal, Pressable, StyleSheet, Text, TouchableOpacity, View} from "react-native";
 import Pdf from "react-native-pdf";
 
 const DATA = [
@@ -24,9 +24,27 @@ const Item = ({ item, onPress, backgroundColor, textColor }) => (
   </TouchableOpacity>
 );
 
+//Float action button object
+function FAB(props){
+    return(
+        <TouchableOpacity style={props.style} onPress={props.onPress}>
+            <View style={{
+                    backgroundColor: "blue",
+                    width: 45,
+                    height: 45,
+                    borderRadius: 45
+                }}>
+                <Text style={{fontSize: 32, color: "white", textAlign: "center", justifyContent: "center"}}>+</Text>
+            </View>
+        </TouchableOpacity>
+    );
+}
+
+
 function DocumentView({navigation}){
     //vars to keep track of pressed or not pressed
     const [selectedId, setSelectedId] = useState(null);
+    const [modalVisible, setModalVisible] = useState(false);
 
     //variable to put PDF url in
     const source = { uri: 'http://samples.leanpub.com/thereactnativebook-sample.pdf', cache: true };
@@ -48,6 +66,37 @@ function DocumentView({navigation}){
     }
     return(
         <View style={styles.container}>
+            {/*Floating menu which appears when adding a new document*/}
+            <Modal
+                    animationType="slide"
+                    transparent={true}
+                    visible={modalVisible}
+                    onRequestClose={() => {
+                      setModalVisible(!modalVisible);
+                    }}
+                  >
+                <View style={styles.centeredView}>
+                  <View style={styles.modalView}>
+
+                    <TouchableOpacity>
+                        <Text>Scan Document</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity>
+                        <Text>Upload Document</Text>
+                    </TouchableOpacity>
+
+                      <Pressable
+                      style={[styles.button, styles.buttonClose]}
+                      onPress={() => setModalVisible(!modalVisible)}
+                    >
+                      <Text style={styles.textStyle}>Cancel</Text>
+                    </Pressable>
+                  </View>
+                </View>
+            </Modal>
+            
+            {/*Main View Starts Here*/}
             <View style={styles.header_list}>
                 {/*List component. Data prop gets passed the array of data. renderItem prop a function which renders items*/}
               <FlatList
@@ -58,10 +107,14 @@ function DocumentView({navigation}){
                 horizontal={true}
               />
             </View>
+            
+            {/*Render PDF here*/}
             <View style={styles.my_canvas}>
                 <Pdf source={source} />
-
             </View>
+            
+            {/*Button to add documents */}
+            <FAB style={styles.fab} onPress={() => setModalVisible(true)}/>
 
         </View>
     )
@@ -95,7 +148,53 @@ const styles = StyleSheet.create({
       flex: 9,
       backgroundColor: "#ebebeb"
 
-    }
+    },
+    fab: {
+        position: "absolute",
+        bottom: 8,
+        right: 8
+    },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
+  }
 });
 
 export default DocumentView
