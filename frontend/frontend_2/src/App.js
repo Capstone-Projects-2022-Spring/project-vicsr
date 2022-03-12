45247d462a473033d23a10846dbcbb8433a070ab
bee-mcc <tuj03791@temple.edu>
2022-03-12 00:58:19 -0500

    added new package for list rendering 2

--- frontend/frontend_2/src/App.js
+++ frontend/frontend_2/src/App.js
@@ -13,7 +13,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
-        <DocumentList documents = {fake_documents} ></DocumentList>
+        <DocumentList documents = {fake_documents} />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
--- frontend/frontend_2/src/components/DocumentList.js
+++ frontend/frontend_2/src/components/DocumentList.js
@@ -1,12 +1,23 @@
+import {FixedSizeList as List} from "react-window"
+
 function DocumentList(props){
   const docs = props.documents;
-  const listItems = URLs.map((URL) =>
-    <li>{URL}</li>
-  );
   return(
-      <ul>{listItems}</ul>
+    <List
+      height={150}
+      itemCount={1000}
+      itemSize={35}
+      width={300}
+    >
+      {Row}
+    </List>
   )
 
 }
+
+const Row = ({ index, style, title, URL }) => (
+  <div style={style}>{title}</div>
+);
+
 
 export default DocumentList
