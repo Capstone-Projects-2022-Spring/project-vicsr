941a1c411c68240a5d996258a073b672a0fd556c
Shuang71671 <77736227+Shuang71671@users.noreply.github.com>
2022-03-22 17:41:43 -0400

    Update DocumentUpload.js

--- frontend/frontend_2/src/components/views/DocumentView/DocumentUpload.js
+++ frontend/frontend_2/src/components/views/DocumentView/DocumentUpload.js
@@ -1,5 +1,6 @@
-
+/*
 export default function DocumentUpload(){
+
     return(
         <div>
             <div>
@@ -8,7 +9,42 @@ export default function DocumentUpload(){
             <div className="option">
                 <p>Upload here</p>
             </div>
-
         </div>
+
     )
+
 }
+*/
+
+import React, { Component } from "react";
+
+class InputPage extends Component {
+
+  render() {
+    return (
+      <div className="input-group">
+        <div className="input-group-prepend">
+          <span className="input-group-text" id="inputGroupFileAddon01">
+            Upload
+          </span>
+        </div>
+        <div className="custom-file">
+          <input
+            type="file"
+            className="custom-file-input"
+            id="inputGroupFile01"
+            aria-describedby="inputGroupFileAddon01"
+          />
+          <label className="custom-file-label" htmlFor="inputGroupFile01">
+            Choose file
+          </label>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default InputPage;
+
+
+
