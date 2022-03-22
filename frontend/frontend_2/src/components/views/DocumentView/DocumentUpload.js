fc07b6d5334da3b59276fa31159cf41d4a1bbeba
Shuang71671 <77736227+Shuang71671@users.noreply.github.com>
2022-03-22 14:17:31 -0400

    Create DocumentUpload.js

---
+++ frontend/frontend_2/src/components/views/DocumentView/DocumentUpload.js
@@ -0,0 +1,14 @@
+
+export default function DocumentUpload(){
+    return(
+        <div>
+            <div>
+                <h1>Document Upload!</h1>
+            </div>
+            <div className="option">
+                <p>Upload here</p>
+            </div>
+
+        </div>
+    )
+}
