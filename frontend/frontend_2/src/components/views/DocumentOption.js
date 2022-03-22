85978c44650bfd2ead15208dc26c77e02b10291a
Shuang71671 <77736227+Shuang71671@users.noreply.github.com>
2022-03-22 14:15:25 -0400

    Create DocumentOption.js

---
+++ frontend/frontend_2/src/components/views/DocumentOption.js
@@ -0,0 +1,19 @@
+
+export default function DocumentOption(){
+    return(
+        <div>
+            <div>
+                <h1>Document Options!</h1>
+            </div>
+            <div className="option">
+                <p>Study style</p>
+            </div>
+            <div className="option">
+                <p>Rename Document</p>
+            </div>
+            <div className="option">
+                <p>Delete Document</p>
+            </div>
+        </div>
+    )
+}
