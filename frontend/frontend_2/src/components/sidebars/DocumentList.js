376f86b608968dd598202073a0034ffa97dd1737
bee-mcc <tuj03791@temple.edu>
2022-03-22 23:35:46 -0400

    working, can get documents from user profile, buggy for right now

--- frontend/frontend_2/src/components/sidebars/DocumentList.js
+++ frontend/frontend_2/src/components/sidebars/DocumentList.js
@@ -1,5 +1,6 @@
 import {FixedSizeList as List} from "react-window";
 import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card"
 import { API_URL, REACT_URL } from './../../config'
 import {useEffect, useRef} from "react";
 import "./DocumentList.css"
@@ -9,11 +10,12 @@ import "./DocumentList.css"
 
 function DocumentList(props){
     //initialize variables to save value of from within useEffect using useRef hook
-    let docsToLoad = useRef();
-    let numDocs = useRef();
+    let docsToLoad = useRef([]);
+    let numDocs = useRef(0);
 
     useEffect( () => {
         //function to get documents (inside useEffects as the asynchronous function)
+        fetchDocuments();
         async function fetchDocuments() {
             try {
                 let docListGetDocsAPIstring = API_URL + "/api/docs/list/"
@@ -31,16 +33,31 @@ function DocumentList(props){
                 console.error(error);
             }
         }
-        fetchDocuments();
-
         console.log(docsToLoad)
-    }, []);
+    }, [docsToLoad]);
 
   const docs = [
         {URL: "www.wqdwqojdfnqwd.com", Title: "German HW"},
         {URL: "www.wqdwqojdfqdwwqdwdwd.com", Title: "German HW 2"},
         {URL: "www.dqwdikcdw.com", Title: "German HW 3"}
     ];
+
+    const Row = ({index}) => (
+        <div className="documentListRowWrapper">
+            <Card>
+                <Card.Title>
+                    Title: {docsToLoad.current[index].filename}
+                </Card.Title>
+                <Card.Body>
+                    <div>URL: {docsToLoad.current[index].file}</div>
+                </Card.Body>
+
+
+            </Card>
+
+        </div>
+
+    );
 
 
   //addDocument should be the responsibility of the upload page that Songyaun is coding, refactor this code into
@@ -67,7 +84,7 @@ function DocumentList(props){
         <div className="mb-4">
         </div>
             <List
-                height={150}
+                height={900}
                 itemCount={numDocs.current}
                 itemSize={35}
                 width={300}
@@ -80,9 +97,7 @@ function DocumentList(props){
 
 }
 
-const Row = ({ index, style }) => (
-  <div style={style}>Row {index}</div>
-);
+
 
 // function renderDocumentItem(document, idx){
 //   return (
