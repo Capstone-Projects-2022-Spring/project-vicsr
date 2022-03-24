64522ea36cbbef7adee0e42d7c8b7f5adf49e9bd
bee-mcc <tuj03791@temple.edu>
2022-03-24 13:21:00 -0400

    prototype one of canvas

--- frontend/frontend_2/src/components/DocumentPage.js
+++ frontend/frontend_2/src/components/DocumentPage.js
@@ -1,25 +1,11 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 import CanvasDraw from "react-canvas-draw";
+import ExampleImage from '../assets/Screen Shot 2022-03-24 at 1.05.06 PM.png'
 
 export default function DocumentPage(){
     return(
         <div>
-            <CanvasDraw/>
-=======
-export default function DocumentPage(){
-    return(
-        <div>
-
->>>>>>> 8e61fd3 (init and add react-canvas-draw package)
-=======
-import CanvasDraw from "react-canvas-draw";
-
-export default function DocumentPage(){
-    return(
-        <div>
-            <CanvasDraw/>
->>>>>>> 710a4e7 (changing pdf renderer to canvas draw object)
+            {/* The 80 at the end of the hex code sets the transparency*/}
+            <CanvasDraw enablePanAndZoom = {false} clampLinesToDocument={true} imgSrc={ExampleImage} canvasHeight={800} canvasWidth={500} brushColor={"#FFFF0080"} catenaryColor={"#FFFF0080"}/>
         </div>
     )
 }
