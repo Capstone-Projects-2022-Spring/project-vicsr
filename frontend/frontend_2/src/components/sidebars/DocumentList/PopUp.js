ba077ea8bda6fb1af27fa2c4384b3a1df571db9b
Alyssa <tui91984@temple.edu>
2022-03-27 18:46:27 -0400

    reformatted add doc form, added validation

--- frontend/frontend_2/src/components/sidebars/DocumentList/PopUp.js
+++ frontend/frontend_2/src/components/sidebars/DocumentList/PopUp.js
@@ -1,14 +1,26 @@
 import Button from "react-bootstrap/Button";
 import React, {useState} from "react";
-import {Modal} from "react-bootstrap";
-import {render} from "react-dom";
-import OptionButton from "./OptionButton";
+import { Modal, Form } from "react-bootstrap";
+import { render } from "react-dom";
+//import OptionButton from "./OptionButton";
+
 
 function PopUp() {
   const [show, setShow] = useState(false);
+  const [validated, setValidated] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const handleSubmit = (event) => {
+
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    setValidated(true);
+    handleClose();
+  }
 
   return (
     <>
@@ -21,91 +33,64 @@ function PopUp() {
           <Modal.Title>Add Document</Modal.Title>
         </Modal.Header>
         <Modal.Body>
+            <Form noValidate validated={validated}>
+                {/*File Name*/}
+                <Form.Group className="mb-3" controlId="formFileName">
+                    <Form.Label>File Name</Form.Label>
+                    <Form.Control type="text" placeholder="Enter file name" required/>
+                    <Form.Control.Feedback type="invalid">
+                        Please provide a file name.
+                    </Form.Control.Feedback>
+                </Form.Group>
 
-  {/*File Upload Button*/}
-          <div className="input-group">
-            <div className="input-group-prepend">
-          {/*<Button className="input-group-text" id="inputGroupFileAddon01">
-            Upload
-          </Button>
-          */}
-            </div>
-            <div className="custom-file">
-              <input type="file" className="custom-file-input" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01"/>
-          {/*<label className="custom-file-label" htmlFor="inputGroupFile01">
-            Choose file
-          </label>*/}
-            </div>
-          </div>
-
-
-  {/*Mode Selection*/}
-          <br></br>
-          <div className="custom-control custom-radio">
-          <input
-            type="radio"
-            className="custom-control-input"
-            id="customControlValidation2"
-            name="radio-stacked"
-            required
-          />
-
-          {/*Doc upload*/}
-          <label
-            className="custom-control-label"
-            htmlFor="customControlValidation2"
-          >
-            Translate Mode
-          </label>
-        </div>
-        <div className="custom-control custom-radio mb-3">
-          <input
-            type="radio"
-            className="custom-control-input"
-            id="customControlValidation3"
-            name="radio-stacked"
-            required
-          />
-          <label
-            className="custom-control-label"
-            htmlFor="customControlValidation3"
-          >
-            Definition Mode
-          </label>
-          <div className="invalid-feedback">
-            Choose one of the Mode
-          </div>
-        </div>
-
-  {/*Language Select*/}
-          <select className="browser-default custom-select2">
-            <option>Choose a Language of the Original Document </option>
-            <option value="1">Chinese</option>
-            <option value="2">English</option>
-            <option value="3">French</option>
-            <option value="4">German</option>
-            <option value="6">Latin</option>
-            <option value="5">Spanish</option>
-          </select>
-
-          <select className="browser-default custom-select2">
-            <option>Choose a Language to Translate</option>
-            <option value="1">Chinese</option>
-            <option value="2">English</option>
-            <option value="3">French</option>
-            <option value="4">German</option>
-            <option value="6">Latin</option>
-            <option value="5">Spanish</option>
-          </select>
+                {/*File Upload*/}
+                <Form.Group className="mb-3" controlId="formFile">
+                    <Form.Label>File</Form.Label>
+                    <Form.Control type="file" placeholder="Browse computer" required/>
+                </Form.Group>
 
 
+                {/*Mode Selection*/}
+                <Form.Group className="mb-3" controlId="formMode">
+                    <Form.Label>Mode</Form.Label>
+                    <Form.Select required>
+                        <option value="">Choose study mode</option>
+                        <option value="TLR">Translation</option>
+                        <option value="DEF">Definition</option>
+                    </Form.Select>
+                </Form.Group>
+
+              {/*Language Select*/}
+              <Form.Group className="mb-3" controlId="formLanguage">
+                  <Form.Label>Language of Original Document</Form.Label>
+                  <Form.Select required className="mb-3">
+                    <option value="">Choose language </option>
+                    <option value="1">Chinese</option>
+                    <option value="2">English</option>
+                    <option value="3">French</option>
+                    <option value="4">German</option>
+                    <option value="6">Latin</option>
+                    <option value="5">Spanish</option>
+                  </Form.Select>
+              </Form.Group>
+
+              {/*Translation Language Select*/}
+              <Form.Group className="mb-3" controlId="formTransLanguage">
+                  <Form.Label>Language to Translate to</Form.Label>
+                  <Form.Select required className="mb-3">
+                    <option value="">Choose language </option>
+                    <option value="1">Chinese</option>
+                    <option value="2">English</option>
+                    <option value="3">French</option>
+                    <option value="4">German</option>
+                    <option value="6">Latin</option>
+                    <option value="5">Spanish</option>
+                  </Form.Select>
+              </Form.Group>
+            </Form>
         </Modal.Body>
-
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
+          <Button variant="primary" onClick={handleSubmit}>
             Upload
           </Button>
         </Modal.Footer>
