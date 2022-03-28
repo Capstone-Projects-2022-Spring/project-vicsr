a7d68f6c560504022e9e8c7651144945019d6aa9
bee-mcc <tuj03791@temple.edu>
2022-03-27 21:05:58 -0400

    updated for rebase

--- frontend/frontend_2/src/components/DocumentPage.js
+++ frontend/frontend_2/src/components/DocumentPage.js
@@ -1,9 +1,16 @@
+<<<<<<< HEAD
 import CanvasDraw from "react-canvas-draw";
 
 export default function DocumentPage(){
     return(
         <div>
             <CanvasDraw/>
+=======
+export default function DocumentPage(){
+    return(
+        <div>
+
+>>>>>>> 8e61fd3 (init and add react-canvas-draw package)
         </div>
     )
 }
