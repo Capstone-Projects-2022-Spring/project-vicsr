f9eee68f6bad427d9547cc56444c520e71f2ba0d
Alyssa <tui91984@temple.edu>
2022-03-28 14:03:04 -0400

    successfully uploads file objects from the front end

--- frontend/frontend_2/src/components/sidebars/DocumentList/PopUp.js
+++ frontend/frontend_2/src/components/sidebars/DocumentList/PopUp.js
@@ -6,44 +6,17 @@ import { render } from "react-dom";
 import {API_URL} from '../../../config'
 
 
-
-function PopUp() {
+function PopUp(){
   const [show, setShow] = useState(false);
   const [validated, setValidated] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-  async function addDocument(document) {
-
-    let addDocumentString = API_URL + '/api/docs/add/'
-    return fetch(addDocumentString, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Token ${sessionStorage.getItem('token')}`
-
-        },
-        body: JSON.stringify(document)
-    })
-        .then(data => data.json())
-  };
+  const [file, setFile] = useState();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("filename: " + e.target.elements.formFileName.value )
-    // console.log("file: " + e.target.elements.formFile.value )
-    console.log("file: " + e.target.formFile.files[0])
-    console.log("mode: " + e.target.elements.formMode.value )
-    console.log("language: " + e.target.elements.formLanguage.value )
-    console.log("trans language: " + e.target.elements.formTransLanguage.value )
-
-    const filename = e.target.elements.formFileName.value;
-    // const file = e.target.elements.formFile.value;
-    const file = e.target.formFile.files[0];
-    const mode = e.target.elements.formMode.value;
-    const language = e.target.elements.formLanguage.value;
-    const trans_language = e.target.elements.formTransLanguage.value;
 
     const form = e.target;
     if (form.checkValidity() === false) {
@@ -51,7 +24,37 @@ function PopUp() {
       e.stopPropagation();
     }
 
-    addDocument({filename, file, mode, language, trans_language})
+    // get user input
+    const filename = e.target.elements.formFileName.value;
+    //file gotten via state on change in form below
+    const mode = e.target.elements.formMode.value;
+    const language = e.target.elements.formLanguage.value;
+    const trans_language = e.target.elements.formTransLanguage.value;
+
+
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", `Token ${sessionStorage.getItem('token')}`);
+    myHeaders.append("Cookie", "csrftoken=afXBilocRuFLnYhMQA7k60LRU9WX5ulNNzbahvbzIevwWZxAmnOWPC8yyoM1TsEC; messages=.eJxtzMEKgzAMgOFXCTlnIoJ3YY-wo0gpNXaRNgFTD3v7uZ29fvD_84wh7G4aKrvHzEg9DT3h03STo8YmpsCPGqWAszZoBru9dco_6pLVDhe6vYyErzOlS7azlA-4ZOUVRCH6_3GVyxdMZixk:1nX2r9:xSWaIiqs4LUJIrfBmjb9cAJ2mSusq5Sevrs0xEzEHDY");
+
+    const formdata = new FormData();
+        formdata.append('filename', filename);
+        formdata.append('file', file, file.name);
+        formdata.append('mode', mode);
+        formdata.append('language', language);
+        formdata.append('trans_language', trans_language);
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: formdata,
+        redirect: 'follow'
+    };
+
+    let addDocumentString = API_URL + '/api/docs/add/';
+    fetch(addDocumentString, requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
 
     setValidated(true);
     handleClose();
@@ -82,7 +85,8 @@ function PopUp() {
                 {/*File Upload*/}
                 <Form.Group className="mb-3" controlId="formFile" >
                     <Form.Label>File</Form.Label>
-                    <Form.Control type="file" placeholder="Browse computer" inputRef={(ref) => {this.file = ref}} required/>
+                    {/*<Form.Control type="file" placeholder="Browse computer" inputRef={(ref) => {this.file = ref}} required/>*/}
+                    <Form.Control type="file" placeholder="Browse computer" onChange={(e) => setFile(e.target.files[0])} required/>
                 </Form.Group>
 
                 {/*Mode Selection*/}
