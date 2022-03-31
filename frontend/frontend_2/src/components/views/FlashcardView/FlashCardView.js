ae2b55dadfbc8662417559d416f9310009b0b3ff
bee-mcc <tuj03791@temple.edu>
2022-03-31 13:37:35 -0400

    dummy card

--- frontend/frontend_2/src/components/views/FlashcardView/FlashCardView.js
+++ frontend/frontend_2/src/components/views/FlashcardView/FlashCardView.js
@@ -1,14 +1,15 @@
 import './FlashCardView.css'
+import FlashCard from "./FlashCard.js"
 
 export default function FlashCardView(){
+
+
     return(
         <div>
             <div>
                 <h1>This is the flashcard page</h1>
             </div>
-            <div className="flashcard">
-                <p>Content here</p>
-            </div>
+            <FlashCard/>
             <form>
                 <button>Press this</button>
             </form>
