0f779ebded8966aaae9659677cc7aa1d6c856321
bee-mcc <tuj03791@temple.edu>
2022-04-02 17:35:33 -0400

    deleted backend

--- backend-vicsr/DocumentManagement/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import DocumentView
-from rest_framework.routers import SimpleRouter
-
-
-urlpatterns = [
-    # path('upload/', DocumentView.as_view(), name='upload'),
-]
--- backend-vicsr/VocabularyManagement/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class VocabularymanagementConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'VocabularyManagement'
